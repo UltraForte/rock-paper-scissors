e6c8d0ae573c161d2a6a8980af59df5465329b9c
Roman <171175821+UltraForte@users.noreply.github.com>
2024-08-01 14:23:37 -0400

    Add code to return a random value from the choices

--- homepage.js
+++ homepage.js
@@ -1,2 +1,15 @@
 // JavaScript for the Rock, Paper, Scissors game.
 console.log("Hello World")
+
+
+const choices = ["rock", "paper", "scissors"];
+
+function getComputerChoice(choices) {
+    let randomIndex = Math.floor(Math.random() * choices.length);
+
+    let choice = choices[randomIndex];
+
+    return choice;
+}
+let choice = getComputerChoice(choices);
+console.log(choice);
