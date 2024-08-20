e0f5517f786425094f34f7fbe9071de33a806a18
Roman <171175821+UltraForte@users.noreply.github.com>
2024-08-20 16:41:17 -0400

    Fully rewrite .js & .html to make game logic work. Add divs and a button to index.html, in HTML. Now the game works with the added buttons.

--- homepage.js
+++ homepage.js
@@ -1,36 +1,73 @@
 // JavaScript for the Rock, Paper, Scissors game.
 console.log("Hello World")
 
-//Play 5 rounds of the Game//
-function playGame() {
-    for (let i = 1; i <= 5; i++) {
 
+const buttonsDiv = document.getElementById("buttons");
+const rockBtn = document.createElement("button");
+rockBtn.textContent = "Rock";
+const paperBtn = document.createElement("button");
+paperBtn.textContent = "Paper";
+const scissorsBtn = document.createElement("button");
+scissorsBtn.textContent = "Scissors";
+
+const startBtn = document.querySelector("#startGame");
+startBtn.addEventListener("click", () => {
+    let welcomeText = document.getElementById("welcomeText");
+    welcomeText.remove();
+    startBtn.remove();
+
+    const gameText = document.querySelector("#gameText");
+    const selectionText = document.createElement("p");
+    selectionText.textContent = "Make your selection:";
+    gameText.appendChild(selectionText);
+
+    buttonsDiv.appendChild(rockBtn);
+    buttonsDiv.appendChild(paperBtn);
+    buttonsDiv.appendChild(scissorsBtn);
+
+
+    rockBtn.addEventListener("click", () => {
+        let playerSelection = "Rock";
+        playRound(playerSelection);
+        console.log("Your score is:", humanScore, "Computer score is:", computerScore);
+        checkScore();
+    });
+    paperBtn.addEventListener("click", () => {
+        let playerSelection = "Paper";
+        playRound(playerSelection);
+        console.log("Your score is:", humanScore, "Computer score is:", computerScore);
+        checkScore();
+    });
+    scissorsBtn.addEventListener("click", () => {
+        let playerSelection = "Scissors";
+        playRound(playerSelection);
+        console.log("Your score is:", humanScore, "Computer score is:", computerScore);
+        checkScore();
+    });
+
+function checkScore() {
+    if (humanScore === 5) {
+        console.log("You Win the Game!");
+//      console.log("Your score is:", humanScore, "Computer score is:", computerScore);
+    } else if (computerScore === 5) {
+        console.log("You lost! Computer Wins the Game!");
+//      console.log("Your score is:", humanScore, "Computer score is:", computerScore);
+    };
+};
+
+});
+//These score variables must be at global scope, 
+//otherwise they will get reset to 0 every round.
+let humanScore = 0
+let computerScore = 0
+
+
+
+
+
+function playRound(playerSelection) {
 
 const choices = ["Rock", "Paper", "Scissors"];
-
-
-//Pseudo Code to getHumanChoice//
-//Assign a variable that will contain the users choice
-//Declare a function that will get the users choice
-//Prompt user to get users choice
-//Print users choice in console log
-
-function getHumanChoice() {
-    let humanChoice = prompt("Submit your choice: \"Rock\" \"Paper\" or \"Scissors\"").toLowerCase();
-        if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
-            //the one line below capitalizes the first letter of the displayed humanChoice//
-            return humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
-        } else {
-            alert("Choose one of the following options: \"Rock\" \"Paper\" or \"Scissors\"");
-            getHumanChoice();
-        };
-};
-let humanChoice = getHumanChoice();
-console.log("You Choose:", humanChoice);
-
-
-
-//Get computers choice//
 function getComputerChoice(choices) {
     let randomIndex = Math.floor(Math.random() * choices.length);
 
@@ -41,11 +78,12 @@ function getComputerChoice(choices) {
 let computerChoice = getComputerChoice(choices);
 console.log("Computer Chooses:", computerChoice);
 
+//Get human choice:
+let humanChoice = playerSelection;
+console.log("You Choose:", humanChoice);
 
-
-//Play 1 round of the game//
-
-function playRound(humanChoice, computerChoice) {
+//Display outcome:
+function displayOutcome(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
         console.log("It's a tie!");
     } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
@@ -72,27 +110,5 @@ function playRound(humanChoice, computerChoice) {
         console.log("There has been en error");
     };
 };
-playRound(humanChoice, computerChoice);
-// rock      beats, scissors
-// scissors  beat paper
-// paper     beats rock
-};
-};
-
-//These score variables must be at global scope, 
-//otherwise they will get reset to 0 every round.
-let humanScore = 0
-let computerScore = 0
-//This call to function starts the game//
-playGame();
-
-if (humanScore > computerScore) {
-    console.log("You Win the Game!");
-    console.log("Your score is:", humanScore, "Computer score is:", computerScore);
-} else if (computerScore > humanScore) {
-    console.log("You lost! Computer Wins the Game!");
-    console.log("Your score is:", humanScore, "Computer score is:", computerScore);
-} else {
-    console.log("The game is a tie!");
-    console.log("Your score is:", humanScore, "Computer score is:", computerScore);
+displayOutcome(humanChoice, computerChoice);
 };
